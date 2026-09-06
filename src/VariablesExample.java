1a75642a8f9f639463b2a2522fdc966ef173f53c
Vileit <vileitq@mail.ru>
2026-09-06 19:43:11 +0300

    add variables example

---
+++ src/VariablesExample.java
@@ -0,0 +1,13 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 1200.50;
+        boolean isActive = true;
+        String name = "Vlad";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Active: " + isActive);
+    }
+}
