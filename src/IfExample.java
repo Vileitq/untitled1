499c9ec8bdfbd497bc384e214f124b342c0e1900
Vileit <vileitq@mail.ru>
2026-09-07 20:42:23 +0300

    add ifExample

---
+++ src/IfExample.java
@@ -0,0 +1,13 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Число положительное");
+        } else if (number <0) {
+            System.out.println("Число отрицательное");
+        } else {
+            System.out.println("Число равно нулю");
+        }
+    }
+}
